3d09b1c78d9f980a1b80a4a65c1e44c7d86eac4b
DevanshG1 <devansh21460@iiitd.ac.in>
2022-11-21 20:10:12 +0530

    Tank

---
+++ core/src/com/mygdx/game/Tank.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Polygon;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.util.*;
+
+public class Tank {
+    private SpriteBatch batch;
+    private Texture Img;
+    private Polygon Hitbox;
+    private double fuel;
+    private ArrayList<Weapons> Weap;
+    public Tank(Texture img){
+        batch=new SpriteBatch();
+        Img=img;
+        Hitbox=new Polygon();
+        fuel=150D;
+        Weap=new ArrayList<Weapons>();
+    }
+    public Texture getimg(){
+        return Img;
+    }
+}
